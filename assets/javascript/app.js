305da3a68685d6d75583317a8d5a769f91c2cd5d
KevinL <mastahkevin@gmail.com>
2019-03-24 21:11:04 -0700

    Styled HTML and added Pseudocode for JS. Added Q's for game.

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+/*
+ -----------------------------------------------------------------
+|   PSEUDO CODE                                                   |
+ -----------------------------------------------------------------
+|   1. Click start button                                         |
+|   2. Q1 appears and timer starts                                |
+|   3. User guesses answer to 4 multi choice questions            |
+|       a. User cannot guess more than 1 answer per question      |
+|           1. In this case use radio buttons                     |
+|   4. User submits answers                                       |
+|       a. If time is left, user is allowed to go ahead and       |
+|          submit answers.                                        |
+|           1. Answers are tallied with the number correct and    |
+|              number wrong.                                      |
+|       b. If time is not left, user loses game.                  |
+-------------------------------------------------------------------
+--------------------------
+|  Qs for baseball trivia |
+---------------------------
+- Who holds the single season record for the highest batting average?
+    - Jose Canseco
+    - Ty Cobb
+    - Hugh Duffy*
+        - Hugh batted a record .440 in 1894.
+    - Fred Dunlap
+- Who holds the record for the fastest pitch in Baseball History?
+    - Aroldis Chapman
+        -  Pitched a whoppihg 105.1 MPH on September 24, 2010
+    - Nolan Ryan
+    - Dennis Eckersley
+    - Tim Wakefield
+- Who holds the record for throwing the most No Hitters in their carrer?
+    - Nolan Ryan*
+        - Nolan Ryan holds the career record with 6 No-nos.
+    - Aroldis Chapman
+    - Sandy Koufax
+    - Jason Varitek
+- How many bases did Rickey Henderson steal in his illustrious career?
+    - 1376
+    - 1406*
+        - Previous record holder was Lou Brock
+    - 1275
+    - 1402
+
+*/
+
+$(document).ready(function() {
+   
+    // delcare variables
+    // tracking number of q's asked.
+    var questionCounter = 0;
+
+    // more stuff
+
+
+    // 
+});
