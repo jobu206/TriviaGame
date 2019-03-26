d478008073ce938ceababb174f2715a52ca4f5e6
KevinL <mastahkevin@gmail.com>
2019-03-26 15:35:30 -0700

    Refined pseudo code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,25 @@
 /*
- -----------------------------------------------------------------
-   PSEUDO CODE                                                   
- -----------------------------------------------------------------
-   1. Click start button                                         
-       a. Need a variable to hold start function             
+-------------------------------------------------------------------------
+   PSEUDO CODE
+-------------------------------------------------------------------------
+   1. Click start button
+        -> Q's are hidden until user clicks start button.
+            -> Have button inserted via jQuery and append to jumbotron.
    2. All Qs appear and timer starts
+        -> Start button is hidden
    3. User guesses answer to 4 multi choice questions
-       a. User cannot guess more than 1 answer per question
-           1. In this case use radio buttons
-                - Need to figure out how to capture answer in variable.
+       -> User cannot guess more than 1 answer per question
+           -> In this case use radio buttons
+                -> Need to figure out how to capture answer in variable.
    4. User submits answers
-       a. If time is left, user is allowed to go ahead and
+        -> If time is left, user is allowed to go ahead and
           submit answers.
-       b. If time is not left, any unaswered Qs marked incorrect
-        c. Answers are tallied with the number correct and
-              number wrong.
+            -> If time is not left, any unaswered Qs marked incorrect
+        -> Answers are tallied with the number correct and number wrong.
 
---------------------------
-  Qs for baseball trivia 
----------------------------
+-------------------------------------------------------------------------
+    Qs for baseball trivia
+-------------------------------------------------------------------------
 - Who holds the single season record for the highest batting average?
     - Jose Canseco
     - Ty Cobb
@@ -48,5 +49,12 @@
 
 $(document).ready(function() {
 
-    // $("#quiz").hide();
+    // hide quiz until start button is clicked.
+    $("#quiz").hide();
+
+    // function to start game
+    
+
+
+
 });
